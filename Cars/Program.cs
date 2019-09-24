5ee10c5534fc4b96f894ee23fe1d16268bbcf1fa
IMAMBAKS <r@imambaks.nl>
2019-09-24 08:27:52 +0200

    updated the Program

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -12,17 +12,58 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var cars = ProcessFile("fuel.csv");
-            var query = cars.OrderByDescending(c => c.Combined);
-            foreach (var car in query.Take(10))
-            {
-                Console.WriteLine($"{car.Name}: {car.Combined}");
-                
-            }
+            var cars = ProcessCars("fuel.csv");
+            var manufacturers = ProcessManufacturers("manufacturers.csv");
 
+
+            var query =
+                from car in cars
+                join manufacturer in manufacturers on car.Manufacturer equals manufacturer.Name
+                orderby car.Combined descending, car.Name ascending
+                select new
+                {
+                    manufacturer.Headquarters,
+                    car.Name,
+                    car.Combined
+                };
+
+            var query2 =
+                cars.Join(manufacturers, c => c.Manufacturer, m => m.Name, (c, m) =>
+                    new
+                    {
+                        m.Headquarters,
+                        c.Name,
+                        c.Combined
+                    })
+                    .OrderByDescending(c => c.Combined)
+                    .ThenBy(c => c.Name);
+
+
+            foreach (var car in query2.Take(10))
+            {
+                Console.WriteLine($"{car.Headquarters} {car.Name}: {car.Combined}");
+            }
         }
 
-        private static List<Car> ProcessFile(string path)
+        private static List<Manufacturer> ProcessManufacturers(string path)
+        {
+            return
+                File.ReadAllLines(path)
+                    .Where(l => l.Length > 1)
+                    .Select(l =>
+                    {
+                        var columns = l.Split(",");
+                        return new Manufacturer
+                        {
+                            Name = columns[0],
+                            Headquarters = columns[1],
+                            Year = int.Parse(columns[2])
+                        };
+                    })
+                    .ToList();
+        }
+
+        private static List<Car> ProcessCars(string path)
         {
             return
                 File.ReadAllLines(path)
