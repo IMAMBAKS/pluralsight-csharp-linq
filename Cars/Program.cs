dd97c56eb472eed36e6cc104d1d1f8563cf17364
IMAMBAKS <r@imambaks.nl>
2019-09-27 09:41:03 +0200

    finished chapter queires

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,38 +16,51 @@ namespace Cars
             var cars = ProcessCars("fuel.csv");
             var manufacturers = ProcessManufacturers("manufacturers.csv");
 
-
             var query =
-                from car in cars
-                join manufacturer in manufacturers on car.Manufacturer equals manufacturer.Name
-                orderby car.Combined descending, car.Name ascending
+                from manufacturer in manufacturers
+                join car in cars on manufacturer.Name equals car.Manufacturer
+                    
+                    into carGroup
+                orderby manufacturer.Name
                 select new
                 {
-                    manufacturer.Headquarters,
-                    car.Name,
-                    car.Combined
+                    Manufacturer = manufacturer,
+                    Cars = carGroup
                 };
 
+
             var query2 =
-                cars.GroupBy(c => c.Manufacturer.ToUpper())
-                    .OrderBy(g => g.Key);
-            
+                cars.GroupBy(c => c.Manufacturer)
+                    .Select(g =>
+                    {
+                        var results = g.Aggregate(new CarStatistics(),
+                            (acc, c) => acc.Accumulate(c),
+                            acc => acc.Compute());
+
+                        return new
+                        {
+                            Name = g.Key,
+                            Avg = results.Average,
+                            Max = results.Max,
+                            Min = results.Min
+                        };
+                    })
+                    .OrderBy(r => r.Max);
+                
 
 
-
-            foreach (var group in query2)
+            foreach (var result in query2)
             {
-                Console.WriteLine(group.Key);
-
-                foreach (var car in group.OrderByDescending(c=>c.Combined))
-                {
-
-                    Console.WriteLine($"\t{car.Name} : {car.Combined}");
-                    
-                }
+                Console.WriteLine($"{ result.Name }");
+                Console.WriteLine($"\tMax: { result.Max }");
+                Console.WriteLine($"\tMin: { result.Min }");
+                Console.WriteLine($"\tAverage: { result.Avg }");
+                
                 
             }
         }
+        
+        
 
         private static List<Manufacturer> ProcessManufacturers(string path)
         {
@@ -74,6 +88,37 @@ namespace Cars
                     .Where(line => line.Length > 1)
                     .Select(Car.ParseFromCsv)
                     .ToList();
+        }
+    }
+
+    public class CarStatistics
+    {
+        public int Max { get; set; }
+        public int Total { get; set; }
+        public int Count { get; set; }
+        public int Min { get; set; }
+        public double Average { get; set; }
+
+        public CarStatistics()
+        {
+            Max = Int32.MinValue;
+            Min = Int32.MaxValue;
+            
+        }
+        public CarStatistics Accumulate(Car car)
+        {
+            Count += 1;
+            Total += car.Combined;
+            Max = Math.Max(Max, car.Combined);
+            Min = Math.Min(Min, car.Combined);
+            return this;
+        }
+
+        public CarStatistics Compute()
+        {
+
+            Average = Total / Count;
+            return this;
         }
     }
 }
